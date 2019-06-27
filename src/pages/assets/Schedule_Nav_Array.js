560a61c010313a90405adde461a3f9f05bf648d9
Adaml45 <adamlutz78@gmail.com>
2019-06-27 11:49:24 -0400

    removed

--- src/pages/assets/Schedule_Nav_Array.js
+++
@@ -1,8 +0,0 @@
-export const liSchedule = [
-  { index: 1, name: "Late", url: "/Late" },
-  { index: 2, name: "Due Within 1 Day ", url: "/One_Day" },
-  { index: 3, name: "7 Days Out ", url: "/7_Days" },
-  { index: 4, name: "14 Days Out ", url: "/14_Days" },
-  { index: 5, name: "New Commitment Date ", url: "/New_Commitment" },
-  { index: 6, name: "Completed Date ", url: "/Completed_Date" }
-];
